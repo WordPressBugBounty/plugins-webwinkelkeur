7a894f0685a82818f2dd98ec641d8dcdd1a4b2c5
Sean Murphy <sean@iamseanmurphy.com>
2026-08-21 13:07:55 +0000

    3.43

--- webwinkelkeur/readme.txt
+++ webwinkelkeur/readme.txt
@@ -78,6 +78,13 @@ add_filter('webwinkelkeur_request_invitation', function ($should_invite, WC_Orde
 
 === Changelog ===
 
+= 3.43 - 2026-08-21 =
+* Add German translations.
+* Generate plugin-specific translation catalogs for WebwinkelKeur and TrustProfile.
+
+= 3.42 - 2026-08-21 =
+* Security: require the `manage_options` capability and a nonce for the plugin's admin AJAX endpoints.
+
 = 3.41 - 2026-08-13 =
 * Bump **Tested up to** to WordPress 7.1.
 
--- webwinkelkeur/webwinkelkeur.php
+++ webwinkelkeur/webwinkelkeur.php
@@ -3,9 +3,11 @@
 Plugin Name: WebwinkelKeur
 Plugin URI: https://www.webwinkelkeur.nl/webwinkel/mogelijkheden/wordpress-module/
 Description: De WordPress plugin zorgt voor een eenvoudige integratie van het WebwinkelKeur binnen jouw webwinkel. Hiermee is het heel eenvoudig om de innovatieve <a href="https://www.webwinkelkeur.nl/webwinkel/mogelijkheden/sidebar/">WebwinkelKeur Sidebar</a> in jouw WordPress website of WooCommerce webwinkel te integreren. Wanneer je WooCommerce gebruikt, kunnen er bovendien automatisch uitnodigingen naar je klanten worden gestuurd.
-Version: 3.41
+Version: 3.43
 Author: Albert Peschar
 Author URI: https://peschar.net/
+Text Domain: webwinkelkeur
+Domain Path: /common/languages
 WC tested up to: 4.99
 */
 
